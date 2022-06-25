3b187492c186a941497974e1ce9292cea26984ca
PC_my <user@gmail.com>
2022-06-25 20:54:59 +0300

    0.1.1.1

---
+++ main_file.py
@@ -0,0 +1,32 @@
+import os
+import logging
+from dotenv import load_dotenv
+from telegram.ext import Updater, MessageHandler, Filters
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+load_dotenv()
+
+
+def echo(update, context):
+    update.message.reply_text(update.message.text)
+
+
+def main():
+    load_dotenv()
+    updater = Updater(os.getenv('TOKEN'))
+    dp = updater.dispatcher
+
+    test = MessageHandler(Filters.text, echo)
+    dp.add_handler(test)
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
